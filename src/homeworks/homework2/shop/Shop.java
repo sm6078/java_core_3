e775dc785ab33d0444b1cd56aa1dba9af08186ef
Kibo <sm6078@mail.ru>
2023-12-06 17:11:56 +0300

    update hw2, checkstyle

--- src/homeworks/homework2/shop/Shop.java
+++ src/homeworks/homework2/shop/Shop.java
@@ -14,12 +14,14 @@ public class Shop {
     public void printWorkers() {
         System.out.println("Список работников магазина:");
         for (Worker worker : workers) {
-            System.out.printf(String.format("Имя работника: %s; Возраст: %s лет; пол: %s;%s", worker.getName(), worker.getAge(),
-                                                            (worker.isMale() ? "мужской" : "женский"), System.lineSeparator()));
+            System.out.printf(String.format("Имя работника: %s; Возраст: %s лет; пол: %s;%s",
+                    worker.getName(), worker.getAge(),
+                    (worker.isMale() ? "мужской" : "женский"), System.lineSeparator()));
             System.out.println("Список вещей:");
             if (worker.getSetOfThings().length > 0) {
                 for (Item item : worker.getSetOfThings()) {
-                    System.out.printf("- %s, %s грамм;%s", item.getName(), item.getWeight(), System.lineSeparator());
+                    System.out.printf("- %s, %s грамм;%s",
+                            item.getName(), item.getWeight(), System.lineSeparator());
                 }
             } else {
                 System.out.println("Вещи отсутсвуют");
